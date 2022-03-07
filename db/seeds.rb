4c423c42ed50b9a4e144b1b7af4c310afce3f988
Diana Jiang <jiang99diana@gmail.com>
2022-03-07 15:23:29 -0500

    incorporated faker seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,172 +3,177 @@ require 'faker'
 puts "🌱 Seeding..."
 
 
-puts "CLearing our tables..."
-# Clearing our tables
-
-
-#-------------------------------------
-#------------ Companies --------------
-puts 'Creating companies...'
-
+puts "Clearing our tables..."
 Company.destroy_all
+Job.destroy_all
+Resume.destroy_all
+User.destroy_all
+Apply.destroy_all
+CoverLetter.destroy_all
+
+
+puts 'Creating companies...'
+#--------------------------------
+#-------- Companies ------------------
 
 company1 = Company.create(
-    
-    image_url: 'http://logok.org/wp-content/uploads/2014/06/Microsoft-logo-m-box.png',
-    name: 'Microsoft',
-    location:'Redmond, WA',
+    image_url: Faker::logo,
+    name: Faker::Company.name Faker::Company.suffix,
+    industry: Faker::Job.field,
     rating: 4
-     
-
 )
 
 company2 = Company.create(
-
-    image_url: 'https://upload.wikimedia.org/wikipedia/commons/a/ab/Apple-logo.png',
-    name: 'Apple',
-    location:'Redmond, WA',
+    image_url: Faker::logo,
+    name: Faker::Company.name Faker::Company.suffix,
+    industry: Faker::Job.field,
     rating: 4
-
 )
 
 company3 = Company.create(
-
-    image_url: 'https://www.designbust.com/download/640/png/facebook_logo_transparent512.png',
-    name: 'Facebook',
-    location:'Menlo Park, CA',
+    image_url: Faker::logo,
+    name: Faker::Company.name Faker::Company.suffix,
+    industry: Faker::Job.field,
     rating: 2
 
 )
 
 company4 = Company.create(
-
-    image_url: 'https://kgo.googleusercontent.com/profile_vrt_raw_bytes_1587515358_10512.png',
-    name: 'Google',
-    location:'Mountain U, CA',
+    image_url: Faker::logo,
+    name: Faker::Company.name Faker::Company.suffix,
+    industry: Faker::Job.field,
     rating: 5
-
 )
 
 #--------------------------------
 #-------- Jobs ------------------
 puts 'Creating Jobs...'
-Job.destroy_all
 
 job1 = Job.create(
-    image_url: 'http://logok.org/wp-content/uploads/2014/06/Microsoft-logo-m-box.png',
+    image_url: Faker::logo,
     title: 'Junior Software Engineer',
-    location: 'Remote/NY',
-    company: company1,
+    location: Faker::Address.state,
     salary: 80000,
+    employment_type: Faker::Job.employment_type,
+    education: Faker::Job.education_level,
     company_id: company1.id
     
 )
 job2 = Job.create(
-     image_url: 'http://logok.org/wp-content/uploads/2014/06/Microsoft-logo-m-box.png',
-     title: 'Level II Software Engineer',
-     location: 'Remote/North Dakota',
-     company: company1,
-     salary: 120000,
-     company_id: company1.id
+    image_url: Faker::logo,
+    title: 'Level II Software Engineer',
+    location: Faker::Address.state,
+    salary: 120000,
+    employment_type: Faker::Job.employment_type,
+    education: Faker::Job.education_level,
+    company_id: company1.id
      
    
     
 )
 job3 = Job.create(
-     image_url: 'http://logok.org/wp-content/uploads/2014/06/Microsoft-logo-m-box.png',
-     title: 'Senior Software Engineer',
-     location: 'Remote/Chicago',
-     company: company1,
-     salary: 175000,
-     company_id: company1.id
+    image_url: Faker::logo,
+    title: 'Software Engineer Intern',
+    location: Faker::Address.state,
+    salary: 175000,
+    employment_type: Faker::Job.employment_type,
+    education: Faker::Job.education_level,
+    company_id: company1.id
    
 )
 
 job4 = Job.create(
-    image_url: 'https://upload.wikimedia.org/wikipedia/commons/a/ab/Apple-logo.png',
+    image_url: Faker::logo,
     title: 'Systems Software Engineer',
-    location: 'Remote/WA',
-    company: company2,
+    location: Faker::Address.state,
     salary: 2000000,
+    employment_type: Faker::Job.employment_type,
+    education: Faker::Job.education_level,
     company_id: company2.id
 
 )
 
 job5 = Job.create(
-    image_url: 'https://upload.wikimedia.org/wikipedia/commons/a/ab/Apple-logo.png',
+    image_url: Faker::logo,
     title: 'Swift Programmer',
-    location: 'Remote/CA',
-    company: company2,
+    location: Faker::Address.state,
     salary: 1000000,
+    employment_type: Faker::Job.employment_type,
+    education: Faker::Job.education_level,
     company_id: company2.id
 
 )
 
 job6 = Job.create(
-    image_url: 'https://upload.wikimedia.org/wikipedia/commons/a/ab/Apple-logo.png',
+    image_url: Faker::logo,
     title: 'AI/ML - iOS & macOS Software Engineer',
-    location: 'Remote/PA',
-    company: company2,
+    location: Faker::Address.state,
     salary: 175000,
+    employment_type: Faker::Job.employment_type,
+    education: Faker::Job.education_level,
     company_id: company2.id
-
 )
 
 job7 = Job.create(
-    image_url: 'https://www.designbust.com/download/640/png/facebook_logo_transparent512.png',
-    title: 'META Software Engineer',
-    location: 'Remote/CA',
-    company: company3,
+    image_url: Faker::logo,
+    title: 'Full-Stack Software Engineer',
+    location: Faker::Address.state,
     salary: 114000,
+    employment_type: Faker::Job.employment_type,
+    education: Faker::Job.education_level,
     company_id: company3.id
 
 )
 
 job8 = Job.create(
-    image_url: 'https://www.designbust.com/download/640/png/facebook_logo_transparent512.png',
+    image_url: Faker::logo,
     title: 'Senior Evil Hack',
-    location: 'Remote/FL',
-    company: company3,
+    location: Faker::Address.state,
     salary: 500000,
+    employment_type: Faker::Job.employment_type,
+    education: Faker::Job.education_level,
     company_id: company3.id
 
 )
 
 job9 = Job.create(
-    image_url: 'https://www.designbust.com/download/640/png/facebook_logo_transparent512.png',
+    image_url: Faker::logo,
     title: 'React Native Developer',
-    location: 'Remote/CA',
-    company: company3,
+    location: Faker::Address.state,
     salary: 130000,
+    employment_type: Faker::Job.employment_type,
+    education: Faker::Job.education_level,
     company_id: company3.id
 
 )
 
 job10 = Job.create(
-    image_url: 'https://kgo.googleusercontent.com/profile_vrt_raw_bytes_1587515358_10512.png',
+    image_url: Faker::logo,
     title: 'Staff Software Engineer, Pixel Graphics',
-    location: 'Remote/WA',
-    company: company4,
+    location: Faker::Address.state,
     salary: 137000,
+    employment_type: Faker::Job.employment_type,
+    education: Faker::Job.education_level,
     company_id: company4.id
 
 )
 job11 = Job.create(
-    image_url: 'https://kgo.googleusercontent.com/profile_vrt_raw_bytes_1587515358_10512.png',
+    image_url: Faker::logo,
     title: 'Junior Software Engineer',
-    location: 'Remote/NY',
-    company: company4,
+    location: Faker::Address.state,
     salary: 80000,
+    employment_type: Faker::Job.employment_type,
+    education: Faker::Job.education_level,
     company_id: company4.id
 
 )
 job12 = Job.create(
-    image_url: 'https://kgo.googleusercontent.com/profile_vrt_raw_bytes_1587515358_10512.png',
+    image_url: Faker::logo,
     title: 'Junior Software Engineer',
-    location: 'Remote/NY',
-    company: company4,
+    location: Faker::Address.state,
     salary: 80000,
+    employment_type: Faker::Job.employment_type,
+    education: Faker::Job.education_level,
     company_id: company4.id
 
 )
@@ -178,7 +183,7 @@ job12 = Job.create(
 
 puts "Creating users..."
 
-User.destroy_all
+
 
 user1 = User.create(
     name: "Adam Bee", 
@@ -216,7 +221,7 @@ user4 = User.create(
 #-------- Resumes ---------------
 
 puts'Creating Resumes...'
-Resume.destroy_all
+
 
 resume1 = Resume.create(  
 
@@ -302,7 +307,7 @@ resume8 = Resume.create(
 #-------- Cover Letters ---------
 
 puts'Creating Cover Letters...'
-CoverLetter.destroy_all
+
 
 coverLetter1 = CoverLetter.create(
 
@@ -366,7 +371,7 @@ coverLetter8 = CoverLetter.create(
 #-------- Apply ----------------- Will come back to this in a moment - MSU
 
 puts 'Creating Applies...'
-Apply.destroy_all
+
 
 apply1 = Apply.create(
     user_id: user1.id,
